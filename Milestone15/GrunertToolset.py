abb088e67a566e5ed5a78ea22f69319ba717c594
HGrunert <herrgrunert@web.de>
2022-11-14 00:07:52 +0100

    fryday update2

--- Milestone15/GrunertToolset.py
+++ Milestone15/GrunertToolset.py
@@ -1,3 +1,4 @@
+import paramiko
 #bitte die E-Book PDF- Seiten (pdf-seite) 198-225 bearbeiten
 #verwende paramiko und urllib um ein tool zu schreiben, das folgende aufgaben erfüllt
 #erstelle für die cli und für die Gui folgende Views:
@@ -64,15 +65,107 @@ def initAll():
 #basis menü (cli oder gui) Ben
 
 def rungui():
-    return(False)
+    initgui()
+    whatopengui()
 
+def whatopengui():
+    global root
+    global frame
+    frame.destroy()
+    frame= tk.Frame(root)
+    labmenu = tk.Label(frame, text= 'Main Menu', font=('times',16))
+    buturl=tk.Button(frame, text= 'UrlLib', command= runurllibgui)
+    butParamiko =tk.Button(frame, text='ParaMiko', command= runparamikogui)
+    butExit = tk.Button(frame, text= 'Exit', command = exit)
+    labmenu.pack()
+    buturl.pack()
+    butParamiko.pack()
+    butExit.pack()
+    frame.pack()
+    root.mainloop()
+
+def runparamikogui():
+    global frame
+    global user
+    user =Sshinput(tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar())
+    frame.destroy()
+    frame = tk.Frame(root)
+    lablab = tk.Label(frame,text= 'Please enter parameters.')
+    labus =tk.Label(frame, text='Username:')
+    labpw =tk.Label(frame, text='Password:')
+    labip =tk.Label(frame, text='Hostname:')
+    labpo =tk.Label(frame, text='Port:')
+    labcmd =tk.Label(frame, text='Command:')
+    entus =tk.Entry(frame,textvariable=user.user)
+    entpw =tk.Entry(frame,textvariable=user.password,show='*')
+    entip =tk.Entry(frame,textvariable=user.ip)
+    entpo =tk.Entry(frame,textvariable=user.port)
+    entcmd =tk.Entry(frame,textvariable=user.cmd)
+    butgoparamiko = tk.Button(frame,text='Execute!',command=goparamiko)
+    butexit= tk.Button(frame,text='Exit',command=exit)
+    lablab.grid(row=0,column=0)
+    labus.grid(row=1,column=0)
+    labpw.grid(row=2,column=0)
+    labip.grid(row=3,column=0)
+    labpo.grid(row=4, column=0)
+    labcmd.grid(row=5,column=0)
+    entus.grid(row=1, column=2)
+    entpw.grid(row=2, column=2)
+    entip.grid(row=3, column=2)
+    entpo.grid(row=4, column=2)
+    entcmd.grid(row=5, column=2)
+    butgoparamiko.grid(row=6,column=0)
+    butexit.grid(row=6,column=2)
+    frame.pack()
+def goparamiko():
+    global user
+    executor= Sshinput(user.user.get(),user.password.get(),user.ip.get(),\
+                       int(user.port.get()),user.cmd.get())
+    executor.execute()
+def gourllib():
+    global url
+    global frame
+    frame.destroy()
+    frame =tk.Frame(root)
+    from urllib import request
+    rueckgabewert = request.urlopen(url.get())
+    # print("Dateigröße in Bytes: ", rueckgabewert.length)
+    inhalt = rueckgabewert.read()
+    lablab = tk.Label(frame,text= 'Type of output:')
+    labtyp = tk.Label(frame,text= type(inhalt))
+    lablab2 = tk.Label(frame,text= 'Content:')
+    inhalt_text = inhalt.decode("UTF-8")
+    labinh =tk.Label(frame, text=inhalt_text)
+    lablab.pack()
+    labtyp.pack()
+    lablab2.pack()
+    labinh.pack()
+    frame.pack()
+def runurllibgui():
+    global frame
+    frame.destroy()
+    frame = tk.Frame(root)
+    global url
+    url = tk.StringVar()
+    laburl = tk.Label(frame, text= 'Enter URL:',font=('times',16))
+    enturl = tk.Entry(frame, textvariable=url)
+    buturl = tk.Button(frame, text= 'Go get it!', command=gourllib)
+    butexit = tk.Button(frame,text='Exit.',command=exit)
+    laburl.pack()
+    enturl.pack()
+    buturl.pack()
+    butexit.pack()
+    frame.pack()
+    return False
+
+
+####################################################
+#region #hauptmenü für die auswahl von urllib oder paramiko
 def runcli():
     while True:
-        whatopen()
-    return(False)
+        whatopencli()
 
-#hauptmenü für die auswahl von urllib oder paramiko
-def whatopen():
+def whatopencli():
     choiche=input('Urllib or Paramiko?')
     if(choiche.lower()=='urllib'):
         runurllibcli()
@@ -94,7 +187,7 @@ def runurllibcli():
         if choiche=='type': print(type(inhalt_text))
         elif choiche=='print': print(inhalt_text)
         elif choiche=='exit': break
-    whatopen()
+
 
 def runparamikocli():
     #user=getpass.getuser()
@@ -105,6 +198,10 @@ def runparamikocli():
     port = input('Enter port or <CR>:') or 2222
     cmd = input('Enter command:') or 'id'
     ssh_command=Sshinput(user,password,ip,port,cmd)
+#endregion
+####################################################
+
+#region #vorgaben?
 #eingabemasken sowohl im cli und gui (einzelabfrage auf einen server)
 #eingabemaske für paramiko (einzelabfrage auf einen server)
 ##CliInputForm()
@@ -130,17 +227,17 @@ def runparamikocli():
 ##makeUrlList()
 #check auf den status der abfrage
 ##checkStatus()
-
-
+#endregion
+###Programm
 ##userchooseGuiCli
-if(input('Do you want a GUI? y/n')=='y'):
+if(input('Do you want a GUI? y/n').lower()=='y'):
     rungui()
 else:
     runcli()
 
-
-def showWebsite():
-    print("Anfang des Inhalts:", rueckgabewert.peek())
+#region #urls, morethings
+#def showWebsite():
+#    print("Anfang des Inhalts:", rueckgabewert.peek())
 '''
 urls =[]
 urls.append('http://www.google.com/')
@@ -174,3 +271,4 @@ for url in urls:
 #Hauptmenu
 ##hauptmenu()
 #cli hauptprogramm
+#endregion
